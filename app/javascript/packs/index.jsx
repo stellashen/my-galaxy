f537e9dfbd7ee2a5fee62dd709b682beae500a21
Yangchen Shen <shyangch@gmail.com>
2018-09-29 15:06:03 -0700

    add root and icon

--- app/javascript/packs/index.jsx
+++ app/javascript/packs/index.jsx
@@ -3,8 +3,6 @@ import ReactDOM from "react-dom";
 import App from "../components/App";
 
 document.addEventListener("DOMContentLoaded", () => {
-  ReactDOM.render(
-    <App />,
-    document.body.appendChild(document.createElement("div"))
-  );
+  const root = document.getElementById("root");
+  ReactDOM.render(<App />, root);
 });
