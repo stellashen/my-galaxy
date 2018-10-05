5429919473b96e4f05c534d1af63897c633ce15f
Yangchen Shen <shyangch@gmail.com>
2018-10-04 17:31:45 -0700

    writing StarItem component

--- app/javascript/components/stars/star_item.jsx
+++ app/javascript/components/stars/star_item.jsx
@@ -2,15 +2,33 @@ import React from "react";
 import Radium from "radium";
 
 const StarItem = ({ star }) => (
-  <div>
-    {star.node.owner.login} / <p style={styles.strong}>{star.node.name}</p>
+  <div style={styles.base}>
+    <a href={star.node.url} target="_blank">
+      <h1 style={styles.h1}>
+        {star.node.owner.login} / <p style={styles.strong}>{star.node.name}</p>
+      </h1>
+    </a>
+    <span style={styles.date}>Starred on {star.starredAt.split("T")[0]}</span>
+    <span> {star.node.description}</span>
   </div>
 );
 
 const styles = {
+  base: {
+    marginBottom: "10px"
+  },
+  h1: {
+    color: "#1E6ED6",
+    ":hover": {
+      textDecoration: "underline"
+    }
+  },
   strong: {
     fontWeight: "900",
     display: "inline"
+  },
+  date: {
+    display: "block"
   }
 };
 
