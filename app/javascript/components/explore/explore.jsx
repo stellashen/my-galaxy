2bd22aa2559f19bd02604a02133010ea99739092
Yangchen Shen <shyangch@gmail.com>
2018-10-07 23:58:24 -0700

    add search query

--- app/javascript/components/explore/explore.jsx
+++ app/javascript/components/explore/explore.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 import Radium from "radium";
+import gql from "graphql-tag";
+import { Query } from "react-apollo";
+import Loading from "../shared/loading";
+
+const SEARCH_REPOS = gql`
+  query Search($keyword: String) {
+    search(query: $keyword, type: REPOSITORY, first: 50) {
+      repositoryCount
+      edges {
+        node {
+          ... on Repository {
+            name
+            descriptionHTML
+            stargazers {
+              totalCount
+            }
+            forkCount
+            updatedAt
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Explore = () => <div>Coming soon!</div>;
 
