53477037d53074d44bcb4290b38f1291220bcf4b
Yangchen Shen <shyangch@gmail.com>
2018-10-08 00:55:24 -0700

    write searchbar

--- app/javascript/components/explore/explore.jsx
+++ app/javascript/components/explore/explore.jsx
@@ -1,56 +1,21 @@
 import React from "react";
 import Radium from "radium";
-import gql from "graphql-tag";
-import { Query } from "react-apollo";
-import Loading from "../shared/loading";
+import Searchbar from "./searchbar";
 
-const SEARCH_REPOS = gql`
-  query Search($keyword: String) {
-    search(query: $keyword, type: REPOSITORY, first: 50) {
-      repositoryCount
-      pageInfo {
-        endCursor
-        hasNextPage
-      }
-      edges {
-        cursor
-        starredAt
-        node {
-          id
-          name
-          owner {
-            id
-            login
-          }
-          descriptionHTML
-          url
-          updatedAt
-          primaryLanguage {
-            id
-            name
-            color
-          }
-          stargazers {
-            totalCount
-          }
-          forkCount
-          viewerHasStarred
-          repositoryTopics(first: 20) {
-            edges {
-              node {
-                topic {
-                  id
-                  name
-                }
-              }
-            }
-          }
-        }
-      }
-    }
+class Explore extends React.Component {
+  render() {
+    return (
+      <div style={styles.base}>
+        <Searchbar />
+      </div>
+    );
   }
-`;
+}
 
-const Explore = () => <div>Coming soon!</div>;
+const styles = {
+  base: {
+    padding: "20px"
+  }
+};
 
 export default Radium(Explore);
