49e6c24a8351b1a7f61f30e7074dcd8fb6e6c8b6
Yangchen Shen <shyangch@gmail.com>
2018-10-09 21:28:39 -0700

    add detail page on the right side

---
+++ app/javascript/components/repo_detail.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Radium from "radium";
+
+class RepoDetail extends React.Component {
+  render() {
+    return <div>detail page</div>;
+  }
+}
+
+const styles = {
+  base: {}
+};
+
+export default Radium(RepoDetail);
--- app/javascript/components/stars/star_list.jsx
+++ app/javascript/components/stars/star_list.jsx
@@ -1,20 +1,40 @@
 import React from "react";
 import Radium from "radium";
 import StarItem from "./star_item";
+import RepoDetail from "../repo_detail";
 
 const StarList = ({ stars, page }) => (
-  <ul style={styles.list}>
-    {stars.map((star, idx) => (
-      <li key={`${idx}${star.cursor}`}>
-        <StarItem star={star} page={page} />
-      </li>
-    ))}
-  </ul>
+  <div style={styles.list}>
+    <section style={styles.leftSection}>
+      <ul>
+        {stars.map((star, idx) => (
+          <li key={`${idx}${star.cursor}`}>
+            <StarItem star={star} page={page} />
+          </li>
+        ))}
+      </ul>
+    </section>
+    <section style={styles.rightSection}>
+      <RepoDetail />
+    </section>
+  </div>
 );
 
 const styles = {
   list: {
-    paddingTop: "20px"
+    paddingTop: "20px",
+    display: "flex"
+  },
+  leftSection: {
+    width: "50%",
+    paddingRight: "20px"
+  },
+  rightSection: {
+    width: "50%",
+    height: "80vh",
+    paddingLeft: "20px",
+    border: "1px solid #d1d5da",
+    borderRadius: "5px"
   }
 };
 
