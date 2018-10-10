9a8924ff448d4ed8b634db2141d56de615fe3ba9
Yangchen Shen <shyangch@gmail.com>
2018-10-10 03:07:52 -0700

    style README

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -12,53 +12,53 @@ class App extends React.Component {
     if (!token) return null;
     return (
       <div id="app" style={styles.base}>
-        <Style
-          scopeSelector="#app-left"
-          rules={{
-            span: {
-              display: "block",
-              margin: "10px"
-            },
-            strong: {
-              fontWeight: "900",
-              display: "inline"
-            },
-            "a:hover": {
-              cursor: "pointer"
-            },
-            i: {
-              marginRight: "5px"
-            },
-            h1: {
-              padding: "6px 12px",
-              color: "#1E6ED6"
-            },
-            "h1:hover": {
-              textDecoration: "underline"
-            },
-            h2: {
-              fontSize: "14px",
-              lineHeight: "60px",
-              padding: "0 20px"
-            },
-            "h2:hover": {
-              background: "#404044"
-            },
-            h5: {
-              display: "inline"
-            },
-            "h5:hover": {
-              textDecoration: "underline"
-            },
-            small: {
-              display: "block",
-              color: "red",
-              fontSize: "12px",
-              padding: "5px 20px"
-            }
-          }}
-        />
         <div id="app-left">
+          <Style
+            scopeSelector="#app-left"
+            rules={{
+              span: {
+                display: "block",
+                margin: "10px"
+              },
+              strong: {
+                fontWeight: "900",
+                display: "inline"
+              },
+              "a:hover": {
+                cursor: "pointer"
+              },
+              i: {
+                marginRight: "5px"
+              },
+              h1: {
+                padding: "6px 12px",
+                color: "#1E6ED6"
+              },
+              "h1:hover": {
+                textDecoration: "underline"
+              },
+              h2: {
+                fontSize: "14px",
+                lineHeight: "60px",
+                padding: "0 20px"
+              },
+              "h2:hover": {
+                background: "#404044"
+              },
+              h5: {
+                display: "inline"
+              },
+              "h5:hover": {
+                textDecoration: "underline"
+              },
+              small: {
+                display: "block",
+                color: "red",
+                fontSize: "12px",
+                padding: "5px 20px"
+              }
+            }}
+          />
           <Navbar />
           <section style={[styles.leftSection, styles.scroll]}>
             <Switch>
