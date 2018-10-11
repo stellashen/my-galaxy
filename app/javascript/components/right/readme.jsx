0616f68196c8fa3c851ff59316c2e40216c67b04
Yangchen Shen <shyangch@gmail.com>
2018-10-10 22:14:56 -0700

    handle relative path in README

--- app/javascript/components/right/readme.jsx
+++ app/javascript/components/right/readme.jsx
@@ -11,33 +11,67 @@ class Readme extends React.Component {
     // some images cannot be rendered, change them to links
 
     // 1. handle Cross-Origin Read Blocking (CORB) for images in Github
-    // example: ![screenshot](https://github...)
+    // example: ![screenshot](https://github...)   absolute path to github
     // Note: don't do this for other sites because images can be uploaded from some sites such as Cloudinary
+
+    // 2. also handle image's relative path to github
+    // example: ![](/images/image.png)
+    // here we convert it to [link to image](/images/image.png)
+    // then it will be found again in step 3
     const array = text.match(/!\[.*?\]\(/g);
     if (array) {
+      let currentIdx = 0;
       array.forEach(el => {
-        const urlStartIdx = text.indexOf(el) + el.length;
+        const urlStartIdx = text.indexOf(el, currentIdx) + el.length;
         const url = text.substring(urlStartIdx, urlStartIdx + 14);
-        if (url === "https://github" || url === "http://github.") {
+        if (
+          url === "https://github" ||
+          url === "http://github." ||
+          url.slice(0, 4) !== "http"
+        ) {
           text = text.replace(el, "[link to image](");
         }
+        currentIdx = urlStartIdx + 1;
       });
     }
-    // 2. handle img tag
+
+    // 3. handle relative path to github
+    const array2 = text.match(/\[.*?\]\(/g);
+    if (array2) {
+      let currentIdx = 0;
+      array2.forEach(el => {
+        const urlStartIdx = text.indexOf(el, currentIdx) + el.length;
+        const urlEndIdx = text.indexOf(")", urlStartIdx);
+        const path = text.substring(urlStartIdx, urlEndIdx);
+        if (path.substring(0, 4) !== "http") {
+          text =
+            text.slice(0, urlStartIdx) +
+            `${this.props.repository.url}/tree/master/` +
+            text.slice(urlStartIdx);
+        }
+        currentIdx = urlEndIdx;
+      });
+    }
+    // 4. handle img tag
     // example: <img src="...">
-    let startIdx = text.indexOf('<img src="');
+    let startIdx = text.indexOf("<img src=");
     while (startIdx !== -1) {
       const endIdx = text.indexOf(">", startIdx);
       const imgText = text.substring(startIdx, endIdx + 1);
-      const relativeUrl = imgText.substring(10, imgText.indexOf('"', 11));
-      if (relativeUrl.substring(0, 4) !== "http") {
-        const absoluteUrl = `${
-          this.props.repository.url
-        }/blob/master/${relativeUrl}`;
-        const newImgText = `<a href="${absoluteUrl}" target="_blank">${absoluteUrl}</a>`;
-        text = text.replace(imgText, newImgText);
+      let urlEnd = imgText.indexOf('" ', 11);
+      if (!urlEnd) {
+        urlEnd = imgText.indexOf("' ", 11);
       }
-
+      if (urlEnd) {
+        const relativeUrl = imgText.substring(10, urlEnd);
+        if (relativeUrl && relativeUrl.substring(0, 4) !== "http") {
+          const absoluteUrl = `${
+            this.props.repository.url
+          }/tree/master/${relativeUrl}`;
+          const newImgText = `<a href="${absoluteUrl}" target="_blank">${absoluteUrl}</a>`;
+          text = text.replace(imgText, newImgText);
+        }
+      }
       startIdx = text.indexOf('<img src="', endIdx);
     }
     return text;
